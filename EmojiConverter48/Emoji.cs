b4a90fddcf2c54b63b29de1c63654fef229ac364
Chris Brandsma <cbrandsma@cspace.com>
2022-03-23 10:41:37 -0600

    initial commit

---
+++ EmojiConverter48/Emoji.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmojiConverter48
+{
+    public class Emoji
+    {
+        public string Codes { get; set; }
+        public string Char { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Group { get; set; }
+        public string SubGroup { get; set; }
+    }
+}
